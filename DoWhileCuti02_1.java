96671b3a6f5699b760a633de835391f740ec013b
aidatulrosida <144088067+aidatulrosida@users.noreply.github.com>
2023-10-10 15:42:01 +0700

    Add files via upload

---
+++ DoWhileCuti02_1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DoWhileCuti02_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti : ");
+        jatahCuti = sc.nextInt();
+
+        do {
+            System.out.print("Apakah anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah hari : ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti : " + jatahCuti);
+                } else if (jatahCuti == 0){
+                    break;
+                }else {
+                    System.out.println("Jatah cuti tidak tersedia, sisa jatah cuti anda tinggal " + jatahCuti);
+                }
+            }
+        } while (jatahCuti > 0);
+    }
+}
