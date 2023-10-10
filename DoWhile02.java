9e8bade199851065cdcdbf9ed429efe1379f658a
aidatulrosida <144088067+aidatulrosida@users.noreply.github.com>
2023-10-10 16:19:18 +0700

    Add files via upload

---
+++ DoWhile02.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class DoWhile02 {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+
+        int x = 0;
+        do {
+            System.out.println(x);
+        } while (++x <= 8);
+        System.out.println("Program berhenti");
+
+        // int x = 10;
+        // do {
+        //    System.out.println(x);
+        // } while (++x <= 8);
+        // System.out.prrintln("Program berhenti");
+    }
+}
